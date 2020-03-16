86d29a94b7efee5206856a40e2d6470fef031ae3
barbaratucker <barbarathomason57@gmail.com>
2020-03-15 20:03:37 -0700

    deploy react app to github pages

---
+++ build/precache-manifest.c80819f9e4ef59f44798e586885eb586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4c77ca0f3ac5771284269c5aa454f82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47493a1dad5218ed9eb5",
+    "url": "/static/css/main.6b9cdf89.chunk.css"
+  },
+  {
+    "revision": "caca1898e09b0b1f003b",
+    "url": "/static/js/2.cf31a966.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.cf31a966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47493a1dad5218ed9eb5",
+    "url": "/static/js/main.0b38f2c5.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
