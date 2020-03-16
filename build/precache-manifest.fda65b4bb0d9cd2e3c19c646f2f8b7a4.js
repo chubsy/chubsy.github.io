5c8efc81ed641bf76bdd2720d48ccdbd48a40eff
barbaratucker <barbarathomason57@gmail.com>
2020-03-15 20:28:23 -0700

    gh-pages

---
+++ build/precache-manifest.fda65b4bb0d9cd2e3c19c646f2f8b7a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f87519149e140dac8fdea3d9bab5084",
+    "url": "/chubsy.github.io/index.html"
+  },
+  {
+    "revision": "e35eea70463cac132803",
+    "url": "/chubsy.github.io/static/css/main.6b9cdf89.chunk.css"
+  },
+  {
+    "revision": "caca1898e09b0b1f003b",
+    "url": "/chubsy.github.io/static/js/2.cf31a966.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/chubsy.github.io/static/js/2.cf31a966.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35eea70463cac132803",
+    "url": "/chubsy.github.io/static/js/main.e12e16f9.chunk.js"
+  },
+  {
+    "revision": "57534bbfad0bf0b06966",
+    "url": "/chubsy.github.io/static/js/runtime-main.a605b1fe.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c80819f9e4ef59f44798e586885eb586.js"
+  "/chubsy.github.io/precache-manifest.fda65b4bb0d9cd2e3c19c646f2f8b7a4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chubsy.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
